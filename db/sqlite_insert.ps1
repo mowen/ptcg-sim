51ced61d76b6cf12c4eafd228830466a643392a4
Martin Owen <martinowenuk@gmail.com>
2025-09-21 20:42:21 +0100

    Removed PowerShell sqlite_insert script, as it missed some actions. Adapted sqlite_insert.sh to Windows.

--- db/sqlite_insert.ps1
+++
@@ -1,9 +0,0 @@
-$ScriptLocation = $PSScriptRoot
-
-$key = (New-Guid).ToString().Replace("-", "").Substring(0, 10)
-$value = (Get-Content "$ScriptLocation/../client/tests/react/reducer/testData/data.json" -Raw) -replace "'", ""
-
-$db = "$ScriptLocation/../server/database/db.sqlite"
-Write-Output "INSERT INTO KeyValuePairs (key, value) VALUES ('$key', '$value');" | sqlite3 $db
-
-Write-Host "http://localhost:4000?importKey=$key"
--- db/sqlite_insert.sh
+++ db/sqlite_insert.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+SQLITE_VER=`sqlite3 --version`
+if [ ! $? -eq 0 ]; then
+    cat <<EOF
+You must download and install sqlite3 for this DB insert script to execute.
+Binaries are available for download here: https://sqlite.org/download.html
+EOF
+    exit 1
+fi
+
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 DB="$SCRIPT_DIR/../server/database/db.sqlite"
@@ -14,8 +23,14 @@ if [ ! -e "$DB" ]; then
     fi
 fi
 
-KEY=`uuidgen | sed 's/[-]//g' | head -c 10`
-VALUE=`cat "$SCRIPT_DIR/../client/tests/react/reducer/testData/data.json" | sed "s/'//g"`
+UUID=`uuidgen 2> /dev/null`
+if [ ! $? -eq 0 ]; then
+    # If no uuidgen then we're probably on Windows
+    UUID=`powershell -Command "[guid]::NewGuid().ToString()"`
+fi
+
+KEY=`echo "$UUID" | sed 's/[-]//g' | head -c 10`
+VALUE=`cat "$SCRIPT_DIR/../client/tests/react/reducer/testData/data.json" | sed "s/'/''/g"`
 
 sqlite3 "$DB" <<EOF 
 CREATE TABLE IF NOT EXISTS KeyValuePairs (key TEXT PRIMARY KEY, value TEXT);
