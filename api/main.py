5ecdb6d7d8273d017d5586272462c4b5d724d10b
Martin Owen <martinowenuk@gmail.com>
2025-12-31 09:25:50 +0000

    Added typed responses to API.

--- api/main.py
+++ api/main.py
@@ -1,15 +1,18 @@
-from typing import Any
-
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
+class MessageResponse(BaseModel):
+    message: str
+
+
 @app.get("/")
-async def root() -> Any:
-    return {"message": "Hello World"}
+async def root() -> MessageResponse:
+    return MessageResponse(message="Hello World")
 
 
 @app.get("/hello/{name}")
-async def say_hello(name: str) -> Any:
-    return {"message": f"Hello {name}"}
+async def say_hello(name: str) -> MessageResponse:
+    return MessageResponse(message=f"Hello {name}")
